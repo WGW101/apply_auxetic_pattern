795b2988da1fa2b860e746e460fb6e5a00b45a45
Guillaume Bono <bonoguillaume@hotmail.fr>
2019-06-06 17:31:24 +0200

    Changed colors handling : no discontinuities between black (no cut) and colored

--- cuts.py
+++ cuts.py
@@ -3,17 +3,15 @@ from topo import undirected, get_opposing_vertex
 from geom import dist, cos_angle
 
 
-def get_cut_ratios(vertices, colors, faces, connected, pattern, red_len, blue_len, green_len, min_dist):
+def get_cut_ratios(vertices, colors, faces, connected, pattern, red_len, green_len, blue_len, min_dist):
     ratios = {}
     for (i, j) in pattern:
         vi, vj = vertices[i], vertices[j]
-        if sum(colors[j][1:]) < 50:
-            ratios[i, j] = 1.0
-            continue
-        l = (colors[j][1] * red_len + colors[j][2] * green_len + colors[j][3] * blue_len) / 255
+        d = dist(vi, vj)
+        l = d - (colors[j][1] * (d - red_len) + colors[j][2] * (d - green_len) + colors[j][3] * (d - blue_len)) / 255
         for f in connected[undirected(i, j)]:
             a = cos_angle(vj, vi, vertices[get_opposing_vertex(faces[f], i, j)])
             if a > 0:
                 l = max(l, min_dist / sqrt(1 - a ** 2))
-        ratios[i, j] = min(1, l / dist(vi, vj))
+        ratios[i, j] = min(1, l / d)
     return ratios
