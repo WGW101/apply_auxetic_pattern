9d611e5c61ea35574239173f8a7b031af68da011
Guillaume Bono <guillaume.bono@inria.fr>
2019-08-06 11:28:21 +0200

    Fixed script not working when no vertexpaint is given in .obj : default mat len left in this case is red-len

--- cuts.py
+++ cuts.py
@@ -8,7 +8,10 @@ def get_cut_ratios(vertices, colors, faces, connected, pattern, red_len, green_l
     for (i, j) in pattern:
         vi, vj = vertices[i], vertices[j]
         d = dist(vi, vj)
-        l = d - (colors[j][1] * (d - red_len) + colors[j][2] * (d - green_len) + colors[j][3] * (d - blue_len)) / 255
+        if colors:
+            l = d - (colors[j][1] * (d - red_len) + colors[j][2] * (d - green_len) + colors[j][3] * (d - blue_len)) / 255
+        else:
+            l = red_len
         for f in connected[undirected(i, j)]:
             a = cos_angle(vj, vi, vertices[get_opposing_vertex(faces[f], i, j)])
             if a > 0:
