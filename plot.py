c9574d427e67d3e3e5990f1dc0d05ed72e918a31
Guillaume Bono <guillaume.bono@inria.fr>
2020-03-14 15:28:54 +0100

    Added inverted pattern in a separate svg roup for inner edges

--- plot.py
+++ plot.py
@@ -8,7 +8,8 @@ def plot_pattern(file_path, uv_coords, faces, uv_faces, cut_ratios, connected, m
     draw.viewbox(0, 0, 1, 1)
     orig_g = draw.g(stroke=svgwrite.rgb(100, 100, 120), stroke_width=0.005)
     pat_g = draw.g(stroke=svgwrite.rgb(0, 0, 0), stroke_width=0.005)
-    complem_g = draw.g(stroke=svgwrite.rgb(0, 0, 255), stroke_width=0.005)
+    border_g = draw.g(stroke=svgwrite.rgb(0, 0, 255), stroke_width=0.005)
+    inv_pat_g = draw.g(stroke=svgwrite.rgb(0, 255, 0), stroke_width=0.005)
 
     for (i, j) in get_all_undirected_edges(faces):
         for uv_i, uv_j in match[i, j]:
@@ -16,7 +17,7 @@ def plot_pattern(file_path, uv_coords, faces, uv_faces, cut_ratios, connected, m
             orig_g.add(draw.line(coord_i, coord_j))
             if (i, j) not in cut_ratios and (j, i) not in cut_ratios \
                     and (len(match[i, j]) == 2 or len(connected[i, j]) == 1):
-                        complem_g.add(draw.line(coord_i, coord_j))
+                        border_g.add(draw.line(coord_i, coord_j))
 
     for (i, j), r in cut_ratios.items():
         for uv_i, uv_j in match[i, j]:
@@ -24,10 +25,13 @@ def plot_pattern(file_path, uv_coords, faces, uv_faces, cut_ratios, connected, m
             cut_point = point_on_seg(coord_j, coord_i, r)
             pat_g.add(draw.line(coord_i, cut_point))
             if len(match[i, j]) == 2 or len(connected[undirected(i, j)]) == 1:
-                complem_g.add(draw.line(cut_point, coord_j))
+                border_g.add(draw.line(cut_point, coord_j))
+            else:
+                inv_pat_g.add(draw.line(cut_point, coord_j))
 
     draw.add(orig_g)
     draw.add(pat_g)
-    draw.add(complem_g)
+    draw.add(border_g)
+    draw.add(inv_pat_g)
     draw.save(True)
 
